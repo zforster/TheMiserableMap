7d698f11ee7b692fff46c0bf8e6b36e4d5b46a41
zforster <zak.forster16@gmail.com>
2020-12-21 22:18:36 +0000

    change favicon and map scheme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import React, { useState, useEffect } from 'react';
+import { MapContainer, useMapEvents, TileLayer, Marker, Popup } from 'react-leaflet'
+import "./App.css"
 
 function App() {
+  const url = `https://api.mapbox.com/styles/v1/themiserablemap/ckiz2m91u7at619rpd9trm2xd/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_API_KEY}`
+  
   return (
-    <h1>miserable</h1>
+    <MapContainer
+    center={{ lat: 51.505, lng: -0.09 }}
+    zoom={6}
+    scrollWheelZoom={true}>
+    <TileLayer
+      url={url}
+    />
+  </MapContainer>
   );
 }
 
