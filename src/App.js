f47efbc68432f68f6b13989cdc639bff59a6768f
zforster <zak.forster16@gmail.com>
2020-12-22 21:13:40 +0000

    set to user location

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import { MapContainer, useMapEvents, TileLayer, Marker, Popup } from 'react-leaflet'
-import "./App.css"
+import { MapContainer, Tooltip, useMap, CircleMarker, useMapEvents, TileLayer, Marker, Popup } from 'react-leaflet'
+import "./App.css";
 
 function App() {
-  const url = `https://api.mapbox.com/styles/v1/themiserablemap/ckiz2m91u7at619rpd9trm2xd/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_API_KEY}`
-  
+  const url = `https://api.mapbox.com/styles/v1/themiserablemap/ckiz2m91u7at619rpd9trm2xd/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_API_KEY}`;
+  const [userLocation, setUserLocation] = useState(null);
+
+  const SetCenterToLocation = () => {
+    const map = useMap();
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        setUserLocation({ lat: lat, lng: lng });
+        map.setView([lat, lng], 6);
+      });
+    }
+    return null;
+  }
+
   return (
-    <MapContainer
-    center={{ lat: 51.505, lng: -0.09 }}
-    zoom={6}
-    scrollWheelZoom={true}>
-    <TileLayer
-      url={url}
-    />
-  </MapContainer>
+    <div style={{display: "flex", flex: 1}}>
+      <MapContainer center={{ lat: 51.505, lng: -0.09 }} zoom={6} scrollWheelZoom={true}>
+         <TileLayer url={url}/>
+         <SetCenterToLocation/>
+      </MapContainer>  
+    </div>
   );
 }
 
